60893638aba7c50a3a1668b3351bd8bd3e18d62b
pieroMC98 <piero11123@hotmail.com>
2020-09-06 01:40:28 +0200

    modelos y controladores creados

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+ */
+Route::resource('/user','User\UserController',['except'=>['create', 'edit']]);
+Route::resource('buyer','Buyer\BuyerController',['only'=>['show','index']]);
+Route::resource('category','Category\CategoryController',['except'=>['create','edit']]);
+Route::resource('product','Product\ProductController',['only'=>['show','index']]);
+Route::resource('transaction','Transaction\TransactionController',['only'=>['show','index']]);
+Route::resource('seller','Seller\SellerController',['only'=>['show','index']]);
