1f633edef3344c215868a523fc0ee55e5ee11c6e
pieroMC98 <piero11123@hotmail.com>
2020-09-12 02:37:22 +0200

    fix handler

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -8,131 +8,142 @@ use Illuminate\Http\Request;
 
 class UserController extends ApiController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return $this->showAll(User::all());
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		return $this->showAll(User::all());
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //no hace falta, ya esta creado
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		//no hace falta, ya esta creado
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $rules = [
-            'name' => 'required',
-            'email'=>['required','email', 'unique:user'],
-            'password'=>['required','min:6','confirmed']
-        ];
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$rules = [
+			'name' => 'required',
+			'email' => ['required', 'email', 'unique:user'],
+			'password' => ['required', 'min:6', 'confirmed'],
+		];
 
-        $this->validate($request,$rules);
-        $value = $request->all();
-        $value['password'] = bcrypt($request->password);
-        $value['verified'] = User::USER_NOT_VERIFIED;
-        $value['admin'] = User::REGULAR;
-        $value['verication_token'] = User::generateToken();
- //       return response()->json(['data'=>User::create($value)],201);
-        return $this->showOne(User::create($value));
-    }
+		$this->validate($request, $rules);
+		$value = $request->all();
+		$value['password'] = bcrypt($request->password);
+		$value['verified'] = User::USER_NOT_VERIFIED;
+		$value['admin'] = User::REGULAR;
+		$value['verication_token'] = User::generateToken();
+		//       return response()->json(['data'=>User::create($value)],201);
+		return $this->showOne(User::create($value));
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        /* return response()->json(['show'=>User::findOrFail($id)]); */
-        return $this->showOne(User::findOrFail($id));
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		/* return response()->json(['show'=>User::findOrFail($id)]); */
+		return $this->showOne(User::findOrFail($id));
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        // no hace falta
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		// no hace falta
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $user = User::findOrFail($id);
-        $rules = [ 
-            'email'=>['email', 'unique:user,email,'.$user->id],
-            'password'=>['min:6','confirmed'],
-            'admin'=> 'in:'.User::REGULAR.','.User::ADMIN
-        ];
-        $this->validate($request,$rules);
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$user = User::findOrFail($id);
+		$rules = [
+			'email' => ['email', 'unique:user,email,' . $user->id],
+			'password' => ['min:6', 'confirmed'],
+			'admin' => 'in:' . User::REGULAR . ',' . User::ADMIN,
+		];
+		$this->validate($request, $rules);
 
-        if($request->has('name')) $user->name = $request->name;
-        
-        if($request->has('email') && $user->email != $request->email){
-            $user->verified = User::USER_NOT_VERIFIED;
-            $user->verification_token = User::generateToken();
-            $user->email = $request->email;
-        } 
+		if ($request->has('name')) {
+			$user->name = $request->name;
+		}
 
-        if($request->has('password'))
-           $user->password = bcrypt($request->password);
+		if ($request->has('email') && $user->email != $request->email) {
+			$user->verified = User::USER_NOT_VERIFIED;
+			$user->verification_token = User::generateToken();
+			$user->email = $request->email;
+		}
 
-        if($request->has('admin')){
-            if($user->isVerified()) 
-                return $this->errorResponse([
-                    'error'=>'only verified user can update his administratior value',
-                    'code' => 409
-                ], 409);
-            $user->admin = $request->admin; 
-        }
+		if ($request->has('password')) {
+			$user->password = bcrypt($request->password);
+		}
 
-        if(!$user->isDirty())
-            return $this->errorResponse([
-                'error' => 'you must update one value at least',
-                'code'=>422
-            ],
-            422);
+		if ($request->has('admin')) {
+			if ($user->isVerified()) {
+				return $this->errorResponse(
+					[
+						'error' =>
+							'only verified user can update his administratior value',
+						'code' => 409,
+					],
+					409
+				);
+			}
+			$user->admin = $request->admin;
+		}
 
-        $user->save();
-        /* return response()->json(['data'=>$user],200); */
-        return $this->showOne($user);
-    }
+		if (!$user->isDirty()) {
+			return $this->errorResponse(
+				[
+					'error' => 'you must update one value at least',
+					'code' => 422,
+				],
+				422
+			);
+		}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $user = User::findOrFail($id);
-        $user->delete();
-    }
+		$user->save();
+		/* return response()->json(['data'=>$user],200); */
+		return $this->showOne($user);
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$user = User::findOrFail($id);
+		$user->delete();
+	}
 }
