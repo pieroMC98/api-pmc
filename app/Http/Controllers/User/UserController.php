0df2a89d42327c0d5046a096830f21ffe15a9513
pieroMC98 <piero11123@hotmail.com>
2020-09-27 23:26:00 +0200

    agreggated implicit injections to User

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -58,10 +58,12 @@ class UserController extends ApiController
 	 * @param  int  $id
 	 * @return \Illuminate\Http\Response
 	 */
-	public function show($id)
+	public function show(User $user)
 	{
 		/* return response()->json(['show'=>User::findOrFail($id)]); */
-		return $this->showOne(User::findOrFail($id));
+		// inyecciones implicitas
+		//return $this->showOne(User::findOrFail($id));
+		return $this->showOne($user);
 	}
 
 	/**
@@ -70,7 +72,7 @@ class UserController extends ApiController
 	 * @param  int  $id
 	 * @return \Illuminate\Http\Response
 	 */
-	public function edit($id)
+	public function edit(User $user)
 	{
 		// no hace falta
 	}
@@ -82,9 +84,9 @@ class UserController extends ApiController
 	 * @param  int  $id
 	 * @return \Illuminate\Http\Response
 	 */
-	public function update(Request $request, $id)
+	public function update(Request $request, User $user)
 	{
-		$user = User::findOrFail($id);
+		//$user = User::findOrFail($id);
 		$rules = [
 			'email' => ['email', 'unique:user,email,' . $user->id],
 			'password' => ['min:6', 'confirmed'],
@@ -141,9 +143,9 @@ class UserController extends ApiController
 	 * @param  int  $id
 	 * @return \Illuminate\Http\Response
 	 */
-	public function destroy($id)
+	public function destroy(User $user)
 	{
-		$user = User::findOrFail($id);
+		//$user = User::findOrFail($id);
 		$user->delete();
 	}
 }
