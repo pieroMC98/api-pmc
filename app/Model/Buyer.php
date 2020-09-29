62ba0f545a3d517405317d130a1d85c7f71c2dac
pieroMC98 <piero11123@hotmail.com>
2020-09-29 20:59:21 +0200

    aggreagte Scopes to Buyer and Seller models

--- app/Model/Buyer.php
+++ app/Model/Buyer.php
@@ -2,13 +2,21 @@
 
 namespace App;
 
+use App\Scope\BuyerScope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Buyer extends User
 {
- //   protected $table = 'buyer';
-    function transaction(){
-        return $this->HasMany(Transaction::class);
-    }
+	//   protected $table = 'buyer';
+
+	protected static function boot()
+	{
+		parent::boot();
+		static::addGlobalScope(new BuyerScope());
+	}
+	function transaction()
+	{
+		return $this->HasMany(Transaction::class);
+	}
 }
--- app/Model/Seller.php
+++ app/Model/Seller.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App;
-
+use App\Scope\SellerScope;
 class Seller extends User
 {
- //   protected $table = 'seller';
-    function product(){
-        return $this->hasMany(Product::class);
-    }
+	//   protected $table = 'seller';
+
+	protected static function boot()
+	{
+		parent::boot();
+		parent::getGlobalScope(new SellerScope());
+	}
+	function product()
+	{
+		return $this->hasMany(Product::class);
+	}
 }
